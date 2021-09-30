9e721ce285bcec3818e44f59606745d830cfd9be
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-30 14:51:45 +0300

    +-

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import './button.css';
+import { useState } from 'react';
 
 
 function App() {
+
+    const [count, setCount] = useState(4);
+
+    function decrementCount() {
+        setCount(count - 1)
+    }
+
+    function incrementCount() {
+        setCount(count + 1)
+    }
+
     return (
         <div>
-            <button className='minus'>-</button>
-            <span>0</span>
-            <button className='plus'>+</button>
+            <button onClick={decrementCount} className='minus'>-</button>
+            <span>{count}</span>
+            <button onClick={incrementCount} className='plus'>+</button>
         </div>
     )
 }
