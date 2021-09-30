6fd1c3d73e6c968c34ec03696808da0ff5fd502f
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-30 14:24:29 +0300

    style

---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './button.css';
+
+
+function App() {
+    return (
+        <div>
+            <button className='minus'>-</button>
+            <span>0</span>
+            <button className='plus'>+</button>
+        </div>
+    )
+}
+
+export default App;
